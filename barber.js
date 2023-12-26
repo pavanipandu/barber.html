600dc55db908f0ceeb210dedf00fab33951ad170
pavanipandu <pavanig121566@gmail.com>
2023-12-26 12:47:22 +0530

    first commit

---
+++ barber.js
@@ -0,0 +1,10 @@
+// const navlist=document.getElementById('navlist')
+// console.log('hello');
+const mblist=document.getElementById('mblist');
+
+const  mbicon=document.getElementById('mbicon')
+mbicon.addEventListener('click',function(){
+    mblist.classList.toggle('showandhide')
+})
+
+
